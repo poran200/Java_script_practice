66656007e8de98e9ca5989c1b019aaf13780c210
Poran chowdury <chowdury659@gmail.com>
2020-08-25 13:02:59 +0600

    search input delay movifightApp

--- moviefightApp/index.js
+++ moviefightApp/index.js
@@ -1,17 +1,29 @@
-console.log('Hi there!');
+console.log("Hi there!");
 
 //  axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=5dec4e6')
 //       .then(({data})=>{
 //           console.log(data)
 //       })
- const getMovies = async ()=>{
-     const response = await axios.get('http://www.omdbapi.com/',{
-         params:{
-             apikey: '5dec4e6',
-             s : 'avengers'
-         }
-     });
-     console.log(response.data);
- }; 
- 
- getMovies();
+const getMovies = async (searchInput) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "5dec4e6",
+      s: searchInput,
+    },
+  });
+  console.log(response.data);
+};
+
+const inputElemnt = document.querySelector("input");
+let timoutId;
+const onInput = (event) => {
+    // if timeOut is not defined mean input is null
+  if (timoutId) {
+      // remove old time out 
+    clearTimeout(timoutId);
+  }
+  timoutId = setTimeout(() => {
+    getMovies(event.target.value.trim());
+  },1000);
+};
+inputElemnt.addEventListener("input", onInput);
