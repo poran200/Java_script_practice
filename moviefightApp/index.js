93e397034adf24988304622c244ef1aeea4e7e0c
Poran chowdury <chowdury659@gmail.com>
2020-08-25 12:07:05 +0600

    timer on root ascess

--- moviefightApp/index.js
+++ moviefightApp/index.js
@@ -1,1 +1,17 @@
 console.log('Hi there!');
+
+//  axios.get('http://www.omdbapi.com/?i=tt3896198&apikey=5dec4e6')
+//       .then(({data})=>{
+//           console.log(data)
+//       })
+ const getMovies = async ()=>{
+     const response = await axios.get('http://www.omdbapi.com/',{
+         params:{
+             apikey: '5dec4e6',
+             s : 'avengers'
+         }
+     });
+     console.log(response.data);
+ }; 
+ 
+ getMovies();
